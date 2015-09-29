73e8691288968bcc3d7d36a20412217d5674073d
Sun Wang <sun.wxg@gmail.com>
2015-09-29 10:20:24 +0800

    add options in help description

--- healthcheck/args.py
+++ healthcheck/args.py
@@ -4,7 +4,11 @@ def args_options():
 
     usage = "usage: %prog [options] [node] < log_file\n" + \
             "      [node] = axe (default)\n" + \
-            "             = mgw"
+            "             = mgw\n" \
+            "      [options]\n" \
+            "             --version\n" \
+            "             --help\n" \
+            "             -h"
 
     parser = OptionParser(usage=usage, version='This %prog version is 0.1')
 
